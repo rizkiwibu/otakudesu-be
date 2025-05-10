bdc5783c0e5f51871fbdea48df0cb4aef3ebf690
Rizki irfan <98686896+rizkiwibu@users.noreply.github.com>
2025-05-11 04:20:06 +0700

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server"
 
 export async function GET() {
   return NextResponse.json([{
-    message: "Otakudesu unofficial API, made by rizkyhaksono with 🤍",
-    GitHub: "https://github.com/rizkyhaksono",
-    Support: "https://saweria.co/natee",
+    message: "Otakudesu unofficial API, made by rizki with 🤍",
+    GitHub: "https://github.com/rizkiwibu",
+    Support: "-",
   },],
     { status: 200 }
   )
